da545e3925d78663c23a936e58f0864076bde29d
martharoman <martha.rxman@gmail.com>
2023-06-27 15:46:36 -0600

    v1

--- db/db.php
+++ db/db.php
@@ -12,15 +12,16 @@ class Conexion
   {
     $host = "127.0.0.1";
     $db = "softrestaurant10pro";
-    $servidor = "sa";
-    $pass = "estadias";
+    $servidor = "sa"; //cambiar al propio
+    $pass = "estadias"; //cambiar al propio
     $puerto = 1433;
 
     try {
 
+        //cambiar al propio info de server
       $cadenaCnx = "sqlsrv: Server=ROMAN\NATIONALSOFT2023;Database=softrestaurant10pro";
       $user = "sa";
-      $password = "estadias";
+      $password = "estadias"; //cambiar al propio
 
       $conexion = new PDO($cadenaCnx, $user, $password);
 
