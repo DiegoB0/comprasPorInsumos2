e8dd4b630f8fb9c8752f6fa15bce766e9a4edf0e
martharoman <118926638+martharoman@users.noreply.github.com>
2023-07-11 23:36:01 -0600

    v5

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,46 +1,50 @@
-$(document).ready(function() {  
-    $('#example').DataTable({        
-        language: {
-                "lengthMenu": "Mostrar _MENU_ registros",
-                "zeroRecords": "No se encontraron resultados",
-                "info": "Mostrando registros del _START_ al _END_ de un total de _TOTAL_ registros",
-                "infoEmpty": "Mostrando registros del 0 al 0 de un total de 0 registros",
-                "infoFiltered": "(filtrado de un total de _MAX_ registros)",
-                "sSearch": "Buscar:",
-                "oPaginate": {
-                    "sFirst": "Primero",
-                    "sLast":"Último",
-                    "sNext":"Siguiente",
-                    "sPrevious": "Anterior"
-			     },
-			     "sProcessing":"Procesando...",
-            },
-        //para usar los botones   
-        responsive: "true",
-        dom: 'Bfrtilp',       
-        buttons:[ 
+$(document).ready(function () {
+	$('#example').DataTable({
+		language: {
+			lengthMenu: 'Mostrar _MENU_ registros',
+			zeroRecords: 'No se encontraron resultados',
+			info: 'Mostrando registros del _START_ al _END_ de un total de _TOTAL_ registros',
+			infoEmpty: 'Mostrando registros del 0 al 0 de un total de 0 registros',
+			infoFiltered: '(filtrado de un total de _MAX_ registros)',
+			sSearch: 'Buscar:',
+			oPaginate: {
+				sFirst: 'Primero',
+				sLast: 'Último',
+				sNext: 'Siguiente',
+				sPrevious: 'Anterior',
+			},
+			sProcessing: 'Procesando...',
+		},
+		//para usar los botones
+		responsive: 'true',
+		dom: 'Bfrtilp',
+		buttons: [
 			{
-				extend:    'excelHtml5',
-				text:      '<i class="fas fa-file-excel"></i> ',
+				extend: 'excelHtml5',
+				text: '<i class="fas fa-file-excel"></i> ',
 				titleAttr: 'Exportar a Excel',
-				className: 'btn btn-success'
+				className: 'btn btn-success',
 			},
 			{
-				extend:    'pdfHtml5',
-				text:      '<i class="fas fa-file-pdf"></i> ',
+				extend: 'pdfHtml5',
+				text: '<i class="fas fa-file-pdf"></i> ',
 				titleAttr: 'Exportar a PDF',
-				className: 'btn btn-danger'
+				className: 'btn btn-danger',
 			},
 			{
-				extend:    'print',
-				text:      '<i class="fa fa-print"></i> ',
+				extend: 'print',
+				text: '<i class="fa fa-print"></i> ',
 				titleAttr: 'Imprimir',
-				className: 'btn btn-info'
+				className: 'btn btn-info',
 			},
-		]	        
-    });
-		/* Datepicker js */
-		$('#datepicker').datepicker({
-			format: 'dd/mm/yyyy',
-		});       
+		],
+	});
+
+	/* 
+	$('#start_date').datepicker({
+		format: 'dd/mm/yyyy',
+	});
+	$('#end_date').datepicker({
+		format: 'dd/mm/yyyy',
+	}); */
 });
