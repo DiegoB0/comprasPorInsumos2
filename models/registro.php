becde1d57dce326b4819fcae21ec4850e601e3c3
martharoman <martha.rxman@gmail.com>
2023-07-12 09:30:14 -0600

    bro

--- models/registro.php
+++ models/registro.php
@@ -23,8 +23,10 @@ class Registro
 			$email=$_POST["email"];
 			$priv=$_POST["priv"];
 
-			$consulta = $db->prepare("INSERT INTO usuarios(nombre, pass, email, privilegio) VALUES (:nombre, :pass, :email, :priv)");
-
+			$consulta = $db->prepare("INSERT INTO usuarios(nombre, pass, email, privilegio) 
+			VALUES (:nombre, (SELECT dbo.fun_encriptar(:pass)), :email, :priv)");
+			//$consulta = $db->prepare("INSERT INTO usuarios(nombre, pass, email, privilegio) 
+			//VALUES (:nombre, :pass, :email, :priv)");
 			$consulta->bindParam(':nombre', $nombre, PDO::PARAM_STR);
 			$consulta->bindParam(':pass', $pass, PDO::PARAM_STR);
 			$consulta->bindParam(':email', $email, PDO::PARAM_STR);
