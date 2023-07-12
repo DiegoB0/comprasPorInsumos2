569acdc9082d5d1fab28459e0fdaa2dd675e9efd
martharoman <118926638+martharoman@users.noreply.github.com>
2023-07-11 23:47:37 -0600

    v5

---
+++ models/registro.php
@@ -0,0 +1,46 @@
+<?php
+
+require_once('../db/loscorrales.php');
+
+
+class Registro
+{
+
+	function __construct()
+	{
+
+	}
+
+	function InsertData()
+	{
+		$conexion = new Corrales();
+		$db = $conexion->con();
+
+		try {
+
+			$nombre=$_POST["nombre"];
+			$pass=$_POST["pass"];
+			$email=$_POST["email"];
+			$priv=$_POST["priv"];
+
+			$consulta = $db->prepare("INSERT INTO usuarios(nombre, pass, email, privilegio) VALUES (:nombre, :pass, :email, :priv)");
+
+			$consulta->bindParam(':nombre', $nombre, PDO::PARAM_STR);
+			$consulta->bindParam(':pass', $pass, PDO::PARAM_STR);
+			$consulta->bindParam(':email', $email, PDO::PARAM_STR);
+			$consulta->bindParam(':priv', $priv, PDO::PARAM_STR);
+
+			$consulta->execute();
+
+			$fila = $consulta->fetch();
+
+			return $fila;
+
+		} catch (PDOException $e) {
+			echo "Error en la consulta->" . $e;
+		}
+	}
+
+}
+
+?>
---
+++ models/reportes.php
@@ -0,0 +1,69 @@
+<?php
+
+require_once('../db/db.php');
+
+
+class Reporte
+{
+
+	function __construct()
+	{
+
+	}
+
+	function fetchData()
+	{
+		$conexion = new Conexion();
+		$db = $conexion->con();
+
+		try {
+			$consulta = $db->prepare("SELECT i.descripcion,
+	SUM(CASE
+	  WHEN m.costo > 0 AND m.cantidad > 0 AND m.idcompra IS NOT NULL THEN m.costo * m.cantidad
+	  ELSE 0
+	END) AS costo,
+	 SUM(CASE
+	  WHEN c.cantidad > 0 AND m.idconcepto = 'EPC' THEN c.cantidad
+	  ELSE 0
+	END) AS cantidadcomprada,
+	(SUM(CASE
+	  WHEN m.cantidad > 0 THEN m.cantidad
+	  ELSE 0
+	END)
+	/NULLIF(SUM(CASE
+	  WHEN c.cantidad > 0 AND m.idconcepto = 'EPC' THEN c.cantidad
+	  ELSE 0
+	END), 0)) AS rendimiento,
+	SUM(CASE
+	  WHEN m.cantidad > 0 THEN m.cantidad
+	  ELSE 0
+	END) AS cantidadcocido,
+	SUM(CASE
+	  WHEN m.cantidad < 0 THEN m.cantidad
+	  ELSE 0
+	END) AS ventas,
+	(SUM(CASE
+	  WHEN m.cantidad > 0 THEN m.cantidad
+	  ELSE 0
+	END)
+	+SUM(CASE
+	  WHEN m.cantidad < 0 THEN m.cantidad
+	  ELSE 0
+	END)) AS inventario_final
+  FROM insumos i
+  LEFT JOIN movsinv m ON i.idinsumo = m.idinsumo
+  LEFT JOIN comprasmovtos c ON i.idinsumo = c.idinsumo
+  GROUP BY i.idinsumo, i.descripcion");
+
+			$consulta->execute();
+
+			$fila = $consulta->fetchAll();
+
+			return $fila;
+
+		} catch (PDOException $e) {
+			echo "Error en la consulta->" . $e;
+		}
+	}
+
+}
